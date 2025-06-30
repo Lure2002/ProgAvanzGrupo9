9b110ff57c464771a2bcda2ea159136240750378
Lucas Contreras <lucastec2002@hotmail.com>
2025-06-30 17:45:35 -0300

    update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import React, { Component } from 'react'
+import Counter from './Counter';
+import Calculator from './Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      //Nosotros empezamos aca
-    </div>
-  );
+const screens = {
+  COUNTER: 1,
+  CALCULATOR: 2
+};
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentScreen: screens.COUNTER
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="button-container">
+          <button className="button" onClick={() => {this.setState({currentScreen : screens.COUNTER})}}>Contador</button>
+          <button className="button" onClick={() => {this.setState({currentScreen : screens.CALCULATOR})}}>Calculadora</button>
+        </div>
+        <br/>
+        {this.state.currentScreen === screens.COUNTER && (<Counter/>)}
+        {this.state.currentScreen === screens.CALCULATOR && (<Calculator/>)}
+      </div>
+    )
+  }
 }
-
-export default App;
